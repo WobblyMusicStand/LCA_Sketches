dc3fb6c4d560de78793b323813945cf526f118b5
WobblyMusicStand <WobblyMusicStand@gmail.com>
2022-04-07 15:47:30 -0700

    Added role assignment LCA sketch

---
+++ Role_Assignment_LCA.ps1
@@ -0,0 +1,12 @@
+﻿$username = "@lab.CloudPortalCredential(admin1).Username"
+#$rgname = "@lab.CloudResourceGroup(corpdata).Name"
+#$SUB = "/subscriptions/" + $(Get-AzSubscription).id
+$SUB = "/subscriptions/@lab.CloudSubscription.Id"
+
+New-AzRoleAssignment -SignInName $username `
+-RoleDefinitionName "Azure Event Hubs Data Owner" `
+-Scope $SUB
+
+Remove-AzRoleAssignment -SignInName $username `
+-RoleDefinitionName "Azure Event Hubs Data Owner" `
+-Scope $SUB
